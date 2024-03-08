8f4ea04a1f93d1d01a53b09a030e8ed5c2d3d23d
Corey Feder <corey.feder@gmail.com>
2024-03-08 11:26:45 -0500

    some routes routed

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-const express = require("express")
+const express = require("express");
 
-const app = express()
-const port = 3000  // try 5000 if troubles
+const app = express();
+const port = 3000; // try 5000 if troubles
 
+app.listen(port, () => {
+    console.log(`Server listening on port: ${port}.`);
+});
+
+// no paths yet, not even '/'. let's declare a route!
+app.get("/", (req, res) => {
+    res.send("Hello Express!");
+});
+
+app.get("/express", (req, res) => {
+    res.send("Creating routes with Express is simple!");
+});
+
+// now let's try other http methods
+app.get("/user", (req, res) => {
+    res.send("Received a GET request for user!");
+});
+
+app.post("/user", (req, res) => {
+    res.send("Received a POST request for user!");
+});
