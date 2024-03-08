75d2d3c1387ed72eb540f0036eeb7fde1ace27a3
Corey Feder <corey.feder@gmail.com>
2024-03-08 12:40:25 -0500

    looking for path to browser console

--- index.js
+++ index.js
@@ -8,10 +8,9 @@ const protocol = "http";
 const url = `${protocol}://${host}:${port}/`
 
 app.listen(port, () => {
-    console.log(`Server listening at: ${url}.`);
+    console.log(`Server listening at:\n  ${url}`);
 });
 
-// no paths yet, not even '/'. let's declare a route!
 app.get("/", (req, res) => {
     res.send("Hello Express!");
 });
@@ -20,10 +19,28 @@ app.get("/express", (req, res) => {
     res.send("Creating routes with Express is simple!");
 });
 
-// now let's try other http methods
 app.get("/user", (req, res) => {
     res.send("Received a GET request for user!");
 });
+
+app.get("/user/:userid", (req, res) => {
+    res.send(`Ah, want to know about user ${req.params.userid}, eh?`);
+});
+
+app.get("/user/:userid/profile", (req, res) => {
+    res.send(`${req.params.userid} is great. We laugh. We cry. Better than <i>Cats</i>.`);
+});
+
+app.get("/user/:userid/profile/:data", (req, res) => {
+    res.send(`Oh no, that'd be an invasion of provacy. ${req.params.userid} probably doesn't want ${req.params.data} out in the open.`);
+});
+
+app.get("/user/:userid/profile/:data", (req, res) => {
+    res.send(
+      `Navigated to the user profile page for: ${req.params.userid}, with the data: ${req.params.data}.`
+    );
+});
+
 
 app.post("/user", (req, res) => {
     res.send("Received a POST request for user!");
@@ -34,5 +51,27 @@ app.patch("/user", (req, res) => {
 });
 
 app.delete("/user", (req, res) => {
-    res.send("HAHA! It actually IS that easy! But don't do that. Murder is bad, y'all.");
+    res.send("HAHA! It actually IS that easy! But don't do that. People, don't do murders.");
 });
+
+app.get("/*", (req, res) => {
+    res.send(`Hello! You have reached the catch-all endpoint!`);
+    // console.group("Request")
+    // console.log(req)
+    // console.groupEnd()
+    // console.group("Response")
+    // console.log(res)
+    // console.groupEnd()
+
+    // trying to get message to browser console
+    // somepathto.userconsole(`Request:\n${req}\n\nResponse:\n${res}`);
+    let test = "Did this show up in the browser console?"
+    // trying:
+    //  res.send(test) [no]
+    //  res.console(test)
+    //  req.send(test)
+    //  req.console(test)
+    //  something .use()
+    //  error pathways
+});
+
